4068edaddd9e4af03209eea0700d53b1d7c35022
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2022-12-29 15:29:30 -0500

    adding functionality for serving static files

--- webserver/app.js
+++ webserver/app.js
@@ -1,20 +1,25 @@
 // const http= require('node:http')
 const express = require('express');
+const path = require('path');
+
 const app = express();
 // hostname and port needed for nodejs while express will ask only for port
 const port = process.env.PORT || 4500;
 
-app.use(express.static('images'));
+app.use(express.static(path.join(__dirname, 'images')));
 
 // serve first request
-app.get('/', (req, res)=>{
+// app.METHOD(PATH, HANDLER)
+app.get('/staticimages', (req, res) => {
   res.send('data from server for get request from / url')
 });
 
-
+app.use('/staticpages', express.static(path.join(__dirname, 'views')));
 
 
 app.listen(port, () => {
   console.log(`Application is running on port ${port}`)
 })
 
+
+app.use('/staticdata',express.static(path.join(__dirname, 'data')));
