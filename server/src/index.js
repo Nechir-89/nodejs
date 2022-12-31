8b4823f66ff346b4c4742cd2f8e00c2ad7a5f3f7
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2022-12-31 08:42:46 -0500

    add user route to get and post users

--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res)=>{
-  res.send('<h1>Server is running</h2>')
+app.get('/', (req, res) => {
+  res.send('<h1>Server is running</h2>');
 })
 
+const usersRoute = require('./routes/users');
+app.use('/api/users', usersRoute);
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
